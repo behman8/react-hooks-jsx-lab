0ef79226cbb82962bb37518c8d7c62ff8fa81482
Branden <brandenehman45@gmail.com>
2021-07-16 08:36:09 -0600

    Completed Lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   return <div id="home">
-    <h1 style={{color: "firebrick"}}>"{name} is a Web Developer from {city}"</h1>
+    <h1 style={{color: "firebrick"}}>{`${name} is a Web Developer from ${city}`}</h1>
   </div>;
 }
 
